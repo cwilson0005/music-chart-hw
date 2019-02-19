0903c09b83f1afb8f41270c5998815db62c6b990
Conor <cwilson0005@hotmail.co.uk>
2019-02-19 23:49:37 +0000

    working on music item

---
+++ src/components/MusicItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MusicItem = (props) => {
+  console.log(props.topSongs);
+  return(
+    <div className="music-item">
+      
+    </div>
+  )
+}
+
+export default MusicItem;
--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,10 +1,26 @@
 import React from "react";
+import MusicItem from "./MusicItem"
 
 const MusicList = (props) => {
-  console.log(props.topSongs);
+  console.log("props:", props.topSongs);
+  const options = props.topSongs.map((song) => {
+    return (
+      <div className="music-list">
+        <h3>Top 20!</h3>
+        <MusicItem
+          // title={props.topSongs.title}
+        />
+      </div>
+    )
+  })
+// props.topSongs is all 20 songs
   return(
     <div className="music-list">
       <h3>Top 20!</h3>
+      <MusicItem
+        // title={props.topSongs.title}
+      />
+      console.log(props.topSongs.title.label);
     </div>
   )
 }

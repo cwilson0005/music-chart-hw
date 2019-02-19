d2a547169ff7049e647521de0b4738e2f43f27d6
Conor <cwilson0005@hotmail.co.uk>
2019-02-19 23:18:00 +0000

    building music list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import MusicBox from "./containers/MusicBox"
 
 class App extends Component {
   render() {
     return (
-
+      <MusicBox />
     );
   }
 }
--- src/containers/MusicBox.js
+++ src/containers/MusicBox.js
@@ -1,7 +1,37 @@
 import React, {Component} from "react";
 
 class MusicBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      topSongs: []
+    };
+    // this.handlePokemonSelected = this.handlePokemonSelected.bind(this);
+  }
 
+  componentDidMount(){
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.addEventListener("load", () => {
+      if (request.status !== 200) return;
+      const jsonString = request.responseText;
+      const data = JSON.parse(jsonString);
+      // data.feed.entry is the top 20 songs
+      this.setState({topSongs: data.feed.entry})
+    });
+    request.send();
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>The Music Box</h2>
+        <MusicList />
+      </div>
+    );
+  }
 }
 
 export default MusicBox;
